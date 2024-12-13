4f4cc054cca408e4d77b9a6467b6fa2ecdf87af8
kanlei <kanlei@live.com>
2024-12-13 16:42:11 +0800

    fix: build

---
+++ src/helper.ts
@@ -0,0 +1,4 @@
+
+export async function helloWorld() {
+  console.log(`hello world`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import { helloWorld } from "./helper.js";
+
 console.log(process.env.KEY);
 console.log(`hello world`);
+
+helloWorld();
